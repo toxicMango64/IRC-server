f0f5e910ad6063de00fa6ec449ea1ecd6168ee57
toxicMango64 <danielfortesque666+github@gmail.com>
2025-05-30 06:07:00 -0700

    removed obsolete files

--- src/server.cpp
+++
@@ -1,9 +0,0 @@
-#include "../inc/Server.hpp"
-#include "../inc/utils.hpp"
-
-Server::Server(int port, const std::string& password) 
-	: port(port), password(password) {}
-
-bool Server::isValid() const {
-	return (port >= MIN_PORT && port <= MAX_PORT) && !password.empty();
-}
