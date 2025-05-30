f95c2f589e7bc3562d1dc3c5c6000fcbf669eea1
toxicMango64 <danielfortesque666+github@gmail.com>
2025-05-30 06:05:08 -0700

    refactored and organized directory includes structure

---
+++ inc/Server.hpp
@@ -0,0 +1,25 @@
+// server.hpp
+#pragma once
+
+#include "Utils.hpp"
+#include <string>
+#include <iostream>
+#include <stdexcept>
+// #include <algorithm>
+
+// enum class e_irc : std::uint16_t {
+enum e_irc {
+	MIN_PORT = 0,
+	MAX_PORT = 65535,
+	MAX_INT_INDEX = 10
+};
+
+class Server {
+private:
+	int port;
+	std::string password;
+
+public:
+	Server(int port, const std::string& password);
+	bool isValid() const;
+};
