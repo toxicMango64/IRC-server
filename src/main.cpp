b0e65f9279ebc8fe112dc4c0aca830ae1fb65d1c
toxicMango64 <danielfortesque666+github@gmail.com>
2025-05-30 06:06:29 -0700

    restructured the project structure

--- src/main.cpp
+++ src/main.cpp
@@ -11,7 +11,6 @@
 */
 
 #include "../inc/Server.hpp"
-#include "../inc/utils.hpp"
 
 // too confusing for others to read
 // 	return (ac != 3) ? (std::cerr << "Usage: " << av[0] << " <port> <password>\n", 1) :
@@ -20,18 +19,25 @@
 // 		   (Server(port, av[2]), std::cout << "Server started on port " << port << "\n", 0);
 
 int main(const int ac, const char * const * av) {
-	if (3 != ac) {
-		std::cerr << "Usage: " << av[0] << " <port> <password>\n";
-		return (1);
-	}
 
 	int port;
-	if (!validatePort(av[1], port) || !validatePassword(av[2])) {
-		return (1);
-	}
+	return (ac != 3) ? (std::cerr << "Usage: " << av[0] << " <port> <password>\n", 1) :
+			(!validatePort(av[1], port = int())) ? 1 :
+			(!validatePassword(av[2])) ? 1 :
+			(Server(port, av[2]), std::cout << "Server started on port " << port << "\n", 0);
 
-	Server server(port, av[2]);
-	std::cout << "[D] Server started on port " << port << "\n";
-	
+			// if (3 != ac) {
+		// 	std::cerr << "Usage: " << av[0] << " <port> <password>\n";
+		// 	return (1);
+		// }
+		
+	// int port;
+	// if (!validatePort(av[1], port) || !validatePassword(av[2])) {
+	// 	return (1);
+	// }
+
+	// Server server(port, av[2]);
+	// std::cout << "[D] Server started on port " << port << "\n";
+
 	return (0);
 }
