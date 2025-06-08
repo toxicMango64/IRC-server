aa9b5cf928ad14543df677c95f57f45c5371f0a9
toxicMango64 <danielfortesque666+github@gmail.com>
2025-06-08 14:10:48 -0700

    refactored Server.hpp for header files being used

--- inc/Server.hpp
+++ inc/Server.hpp
@@ -1,12 +1,19 @@
-// server.hpp
+// Server.hpp
 #pragma once
 
-#include "Utils.hpp"
-#include <string>
-#include <iostream>
-#include <stdexcept>
-#include <stdint.h>
-// #include <algorithm>
+# include "Utils.hpp"
+# include <string>
+# include <iostream>
+# include <stdexcept>
+# include <stdint.h>
+# include <vector>
+# include <poll.h>
+# include <fcntl.h>
+# include <unistd.h>
+# include <netinet/in.h>
+# include <sys/socket.h>
+# include <arpa/inet.h>
+# include <cstring>
 
 // enum class e_irc : std::uint16_t {
 enum e_irc {
