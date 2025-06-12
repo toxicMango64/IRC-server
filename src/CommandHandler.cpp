996a4f2c3c3fd6d4bd7e00d34e61a13e1fa0f7cf
toxicMango64 <danielfortesque666+github@gmail.com>
2025-06-12 02:26:17 -0700

    fix: comparision between the commands

--- src/CommandHandler.cpp
+++ src/CommandHandler.cpp
@@ -1,41 +1,53 @@
 #include "../inc/CommandHandler.hpp"
 
 Commands    getCmd(const char buf[512]) {
-    const std::string command(buf);
+    std::string command(buf);
+    if (command[0] == '/')
+        command = command.substr(1);
     const std::string command_prefix = command.substr(0, 4);
 
-    if (command_prefix == "pass") {
+    if (command_prefix == "PASS") {
         return (PASS);
-    } else if (command_prefix == "user") {
+    } else if (command_prefix == "USER") {
         return (USER);
-    } else if (command_prefix == "nick") {
+    } else if (command_prefix == "NICK") {
         return (NICK);
-    } else if (command_prefix == "join") {
+    } else if (command_prefix == "JOIN") {
         return (JOIN);
-    } else if (command_prefix == "part") {
+    } else if (command_prefix == "PART") {
         return (PART);
+    } else if (command_prefix == "PRIVMSG") {
+        return (PRIVMSG);
     }
     return (INVALID);
 }
 
-void    handleBuffer(Client& client, const char buf[512], std::string& output) {
+void    handleBuffer(Client& client, const char buf[512], const std::string& password, std::string& output) {
     if (client.state == UNAUTHENTICATED) {
         if (getCmd(buf) != PASS) {
             output = "Please enter the password\r\n";
             return ;
         }
         // Compare password with correct one here
-        client.state = AUTHENTICATED;
-        output = "User authenticated\r\n";
+        std::string command(buf);
+        std::string providedPassword = command.substr(5);
+        if (providedPassword == password) {
+            client.state = AUTHENTICATED;
+            output = "User authenticated\r\n";
+        } else {
+            output = "Incorrect password\r\n";
+            return;
+        }
     } else if (client.state == AUTHENTICATED) {
         // Check if user enters USER and NICK here
         if (getCmd(buf) == NICK) {
-            client.nickname = std::string(buf).substr(5);
-            output = client.nickname;
-        }
-        if (getCmd(buf) == USER) {
-            client.username = std::string(buf).substr(5);
-            output = client.username;
+            std::string command(buf);
+            client.nickname = command.substr(5);
+            output = client.nickname + "\r\n";
+        } else if (getCmd(buf) == USER) {
+            std::string command(buf);
+            client.username = command.substr(5);
+            output = client.username + "\r\n";
         }
         if (!client.nickname.empty() && !client.username.empty()) {
             client.state = REGISTERED;
@@ -43,5 +55,17 @@ void    handleBuffer(Client& client, const char buf[512], std::string& output) {
         }
     } else if (client.state == REGISTERED) {
         // Handle normal commands here
+        if (getCmd(buf) == PRIVMSG) {
+            std::string command(buf);
+            size_t pos = command.find(":");
+            if (pos != std::string::npos) {
+                std::string target = command.substr(8, pos - 8 - 1);
+                std::string message = command.substr(pos + 1);
+                // TODO: Send message to target
+                output = "Sending message to " + target + ": " + message + "\r\n";
+            } else {
+                output = "Invalid PRIVMSG format\r\n";
+            }
+        }
     }
 }
