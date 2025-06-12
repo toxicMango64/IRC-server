c47a89d49927ad5b945a977e7a4efad2f671787b
toxicMango64 <danielfortesque666+github@gmail.com>
2025-06-12 01:45:21 -0700

    modular code

--- src/Server.cpp
+++ src/Server.cpp
@@ -44,7 +44,7 @@ void Server::handleNewConnection(int sFd, std::vector<pollfd>& fds) {
         setNonBlocking(client_fd);
         pollfd client_poll{client_fd, POLLIN, 0};
         fds.push_back(client_poll);
-        connectedClients.insert({client_fd, Client(client_fd)});
+        connectedClients.insert(std::make_pair(client_fd, Client(client_fd)));
         debugPrint("New client connected.");
         const std::string welcome = ":ircserv 001 client :Welcome to ft_irc\r\n";
         send(client_fd, welcome.c_str(), welcome.length(), 0);
@@ -95,83 +95,3 @@ void Server::run() {
         }
     }
 }
-
-// void Server::run() {
-// 	const int sFd = socket(AF_INET, SOCK_STREAM, 0);
-// 	if (sFd < 0) {
-// 		throw std::runtime_error("Failed to create socket");
-// 	}
-
-// 	// Make server socket non-blocking
-// 	if (fcntl(sFd, F_SETFL, static_cast<uint64_t>(fcntl(sFd, F_GETFL)) | static_cast<uint64_t>(O_NONBLOCK)) < 0) {
-// 		throw std::runtime_error("Failed to set non-blocking");
-// 	}
-
-// 	// Bind server socket
-// 	struct sockaddr_in addr;
-// 	std::memset(&addr, 0, sizeof(addr));
-// 	addr.sin_family = AF_INET;
-// 	addr.sin_addr.s_addr = INADDR_ANY;
-// 	addr.sin_port = htons(this->_port);
-
-// 	const struct sockaddr *sockAddr = reinterpret_cast<struct sockaddr*>(&addr);
-// 	if (bind(sFd, sockAddr, sizeof(addr)) < 0) {
-// 		throw std::runtime_error("Bind failed");
-// 	}
-
-// 	if (listen(sFd, SOMAXCONN) < 0) {
-// 		throw std::runtime_error("Listen failed");
-// 	}
-
-// 	std::vector<pollfd> fds;
-// 	pollfd server_poll;
-// 	server_poll.fd = sFd;
-// 	server_poll.events = POLLIN;
-// 	server_poll.revents = 0;
-// 	fds.push_back(server_poll);
-
-// 	char buffer[512];
-
-// 	while (true) {
-// 		const int poll_count = poll(&fds[0], fds.size(), -1);
-// 		if (poll_count < 0) {
-// 			throw std::runtime_error("Poll failed");
-// 		}
-
-// 		for (size_t i = 0; i < fds.size(); ++i) {
-// 			if ((static_cast<unsigned int>(fds[i].revents) & static_cast<unsigned int>(POLLIN)) != 0) {
-// 				if (fds[i].fd == sFd) {
-// 					const int client_fd = accept(sFd, NULL, NULL);
-// 					if (client_fd > 0) {
-// 						fcntl(client_fd, F_SETFL, static_cast<uint64_t>(fcntl(client_fd, F_GETFL)) | static_cast<uint64_t>(O_NONBLOCK));
-// 						pollfd client_poll;
-// 						client_poll.fd = client_fd;
-// 						client_poll.events = POLLIN;
-// 						client_poll.revents = 0;
-// 						fds.push_back(client_poll);
-//                         connectedClients.insert(std::make_pair(client_fd, Client(client_fd)));
-// 						debugPrint("New client connected.");
-// 						const std::string welcome = ":ircserv 001 client :Welcome to ft_irc\r\n";
-// 						send(client_fd, welcome.c_str(), welcome.length(), 0);
-// 					}
-// 				} else {
-// 					const int n = static_cast<int>(recv(fds[i].fd, buffer, sizeof(buffer) - 1, 0));
-// 					if (n <= 0) {
-// 						close(fds[i].fd);
-// 						fds.erase(fds.begin() + static_cast<int64_t>(i));
-//                         connectedClients.erase(fds[i].fd);
-// 						--i;
-// 					} else {
-//                         std::string output;
-// 						buffer[n] = '\0';
-// 						std::cout << ">> " << buffer;
-//                         handleBuffer(connectedClients.at(fds[i].fd), buffer, output);
-//                         if (!output.empty()){
-//                             send(fds[i].fd, output.c_str(), output.length(), 0);
-//                         }
-// 					}
-// 				}
-// 			}
-// 		}
-// 	}
-// }
