5bd409dfc1bc498742f6af89df726320625d12d6
NuhaOsman <nuhaosman249@gmail.com>
2025-06-16 05:30:56 +0400

    fix: for @toxicMango's server.hpp prototypes

--- inc/Server.hpp
+++ inc/Server.hpp
@@ -18,19 +18,21 @@
   #define EXPORT
 #endif
 
-#include <arpa/inet.h>
 #include <cstring>
-#include <fcntl.h>
 #include <map>
-#include <netinet/in.h>
-#include <poll.h>
 #include <string>
-#include <sys/socket.h>
-#include <unistd.h>
 #include <vector>
+#include <fcntl.h>
+#include <poll.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
 
-#include "CommandHandler.hpp"
 #include "Client.hpp"
+#include "Channel.hpp"
+#include "CommandHandler.hpp"
+#include "Response.hpp"
 
 // enum class e_irc : std::uint16_t {
 enum e_irc {
@@ -47,44 +49,118 @@ class Client;
 class Channel;
 
 class Server {
-	public:
-		static const int	MAX_BUF = 512;
-		static const char	buffer[MAX_BUF] __attribute__((aligned(16)));
+private:
+	// static const int MAX_BUF = 512;
+	int _port;
+	std::string _password;
+	static bool Signal;
 
-		bool				isValid( void ) const;
-		static void			signalHandler(int signum);
+	int							sfds;
+	std::map<int, Client>		connectedClients;
+	std::vector<Client>			clients;
+	std::vector<Channel>		channels;
+	std::vector<struct pollfd>	fds;
+	struct sockaddr_in			add;
+	struct sockaddr_in			cliaddr;
+	struct pollfd				new_cli;
 
-		Server();
-		~Server();
-		Server( Server const &src );
-		Server &operator=( Server const &src );
-		Server( int port, const std::string& password );
+public:
+	static const int	MAX_BUF = 512;
+	static const char	buffer[MAX_BUF] __attribute__((aligned(16)));
 
-		void	run( int sFd );
-		void	closeFds( void ); /** implement the fucntion */
-		int		createSocket( void );
-		void	bindSocket( int sFd );
-		int		setNonBlocking( int fd );
-		void	startListening( int sFd );
-		void	handleClientMessage(size_t i, std::vector<pollfd>& fds);
-		void	handleNewConnection( int sFd, std::vector<pollfd>& fds );
+	bool				isValid( void ) const;
+	static void			signalHandler(int signum);
 
-		const int& getPort() const { return _port; }
-		const std::string& getPassword() const { return _password; }
-		static void SignalHandler( int signum );
+	Server();
+	~Server();
+	Server( Server const &src );
+	Server &operator=( Server const &src );
+	Server( int port, const std::string& password );
 
-	private:
-		// static const int MAX_BUF = 512;
-		const int _port;
-		const std::string _password;
-		static bool Signal;
+	void	run( int sFd );
+	void	closeFds( void ); /** implement the fucntion */
+	static void SignalHandler( int signum );
+	int		createSocket( void );
+	void	bindSocket( int sFd );
+	int		setNonBlocking( int fd );
+	void	startListening( int sFd );
+	void	handleClientMessage( size_t i, std::vector<pollfd>& fds );
+	void	handleNewConnection( int sFd, std::vector<pollfd>& fds );
 
-		std::map<int, Client> connectedClients;
-		int server_fdsocket;
-		std::vector<Client> clients;
-		std::vector<Channel> channels;
-		std::vector<struct pollfd> fds;
-		struct sockaddr_in add;
-		struct sockaddr_in cliaddr;
-		struct pollfd new_cli;
+	// static bool	isBotfull;
+	int	GetPort( );
+	int	GetFd( );
+	const int&	getPort( ) const { return _port; }
+	Client	*GetClient( int fd );
+	Client	*GetClientNick( std::string nickname );
+	Channel	*GetChannel( std::string name );
+	std::string	GetPassword( );
+
+	void	SetFd( int sfds );
+	void	SetPort( int port );
+	void	SetPassword( std::string password );
+	void	AddClient( Client newClient );
+	void	AddChannel( Channel newChannel );
+	void	AddFds( pollfd newFd );
+	void	set_username( std::string& username, int fd );
+	void	set_nickname( std::string cmd, int fd );
+
+	void	RemoveClient( int fd );
+	void	RemoveChannel( std::string name );
+	void	RemoveFds( int fd );
+	void	RmChannels( int fd );
+
+	void	senderror( int code, std::string clientname, int fd, std::string msg );
+	void	senderror( int code, std::string clientname, std::string channelname, int fd, std::string msg );
+	void	_sendResponse( std::string response, int fd );
+
+	void	init( int port, std::string pass );
+	void	accept_new_client( );
+	void	set_sever_socket( );
+	void	reciveNewData( int fd );
+
+	void	parse_exec_cmd( std::string &cmd, int fd );
+	std::vector<std::string>	split_recivedBuffer( std::string str );
+	std::vector<std::string>	split_cmd( std::string &str );
+
+	bool BypassForBot( std::string cmd, int fd );
+	bool notregistered( int fd );
+	bool nickNameInUse( std::string& nickname );
+	bool is_validNickname( std::string& nickname );
+	void client_authen( int fd, std::string pass );
+
+	void	JOIN( std::string cmd, int fd );
+	int		SplitJoin( std::vector<std::pair<std::string, std::string> > &token, std::string cmd, int fd );
+	void	ExistCh( std::vector<std::pair<std::string, std::string> >&token, int i, int j, int fd );
+	void	NotExistCh( std::vector<std::pair<std::string, std::string> >&token, int i, int fd );
+	int		SearchForClients( std::string nickname );
+
+	void	PART( std::string cmd, int fd );
+	int		SplitCmdPart( std::string cmd, std::vector<std::string> &tmp, std::string &reason, int fd );
+
+	void	KICK( std::string cmd, int fd );
+	std::string SplitCmdKick( std::string cmd, std::vector<std::string> &tmp, std::string &user, int fd );
+
+	void	PRIVMSG( std::string cmd, int fd );
+	void	CheckForChannels_Clients( std::vector<std::string> &tmp, int fd );
+
+	void	QUIT( std::string cmd, int fd );
+
+	void 		mode_command( std::string& cmd, int fd );
+	std::string invite_only( Channel *channel, char opera, std::string chain );
+	std::string topicRestricted( Channel *channel ,char opera, std::string chain );
+	std::string password_mode( std::vector<std::string> splited, Channel *channel, size_t &pos, char opera, int fd, std::string chain, std::string& arguments );
+	std::string operator_privilege( std::vector<std::string> splited, Channel *channel, size_t& pos, int fd, char opera, std::string chain, std::string& arguments );
+	std::string channel_limit( std::vector<std::string> splited, Channel *channel, size_t &pos, char opera, int fd, std::string chain, std::string& arguments );
+	bool	isvalid_limit( std::string& limit );
+	std::string	mode_toAppend( std::string chain, char opera, char mode );
+	std::vector<std::string>	splitParams( std::string params );
+	void	getCmdArgs( std::string cmd,std::string& name, std::string& modeset ,std::string &params );
+
+	std::string tTopic(  );
+	void Topic( std::string &cmd, int &fd );
+	void Invite( std::string &cmd, int &fd );
+	std::string gettopic( std::string& input );
+	int getpos( std::string &cmd );
+
 };
