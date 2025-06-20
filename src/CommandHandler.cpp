07bf75eda564d866fbf51f84ded3b72cfc27127c
toxicMango64 <danielfortesque666@protonmail.com>
2025-06-20 22:41:25 +0400

    better welcome messages

--- src/CommandHandler.cpp
+++ src/CommandHandler.cpp
@@ -1,38 +1,6 @@
 #include "CommandHandler.hpp"
 #include "Response.hpp"
 
-// Commands getCmd(const char buf[MAX_BUF]) {
-//     if (!buf || std::strlen(buf) == 0) {
-//         return INVALID;
-//     }
-
-//     std::string command(buf);
-
-//     if (!command.empty() && command[0] == '/') {
-//         command.erase(0, 1);
-//     }
-
-//     // Convert commands to uppercase CHANGE LATER
-//     std::transform(command.begin(), command.end(), command.begin(), ::toupper);
-
-//     // Extract the command keyword (up to first space or end of string)
-//     size_t spacePos = command.find(' ');
-//     std::string command_keyword = command.substr(0, spacePos);
-
-//     static const std::unordered_map<std::string, Commands> command_map = {
-//         {"PASS", PASS},
-//         {"USER", USER},
-//         {"NICK", NICK},
-//         {"JOIN", JOIN},
-//         {"PART", PART},
-//         {"PRIVMSG", PRIVMSG}
-//     };
-
-//     auto it = command_map.find(command_keyword);
-//     return (it != command_map.end()) ? it->second : INVALID;
-// }
-
-// std::vector<std::string> Server::splitReceivedBuffer(const std::string& str) {
 std::vector<std::string> splitReceivedBuffer(const std::string& str) {
 	std::vector<std::string> vec;
 	std::istringstream stm(str);
@@ -46,7 +14,6 @@ std::vector<std::string> splitReceivedBuffer(const std::string& str) {
 	return vec;
 }
 
-// std::vector<std::string> splitCmd(std::string& cmd)
 std::vector<std::string> Server::splitCmd(std::string& cmd)
 {
 	std::vector<std::string> vec;
@@ -59,77 +26,6 @@ std::vector<std::string> Server::splitCmd(std::string& cmd)
 	}
 	return vec;
 }
-
-// Commands    getCmd(const char buf[Server::MAX_BUF]) {
-//     std::string command(buf);
-//     if (command[0] == '/')
-//         command = command.substr(1);
-//     std::string command_prefix = command.substr(0, 4);
-//     std::transform(command_prefix.begin(), command_prefix.end(), command_prefix.begin(), ::toupper);
-
-//     if (command_prefix == "PASS") {
-//         return (PASS);
-//     } else if (command_prefix == "USER") {
-//         return (USER);
-//     } else if (command_prefix == "NICK") {
-//         return (NICK);
-//     } else if (command_prefix == "JOIN") {
-//         return (JOIN);
-//     } else if (command_prefix == "PART") {
-//         return (PART);
-//     } else if (command_prefix == "PRIVMSG") {
-//         return (PRIVMSG);
-//     }
-//     return (INVALID);
-// }
-
-// void    Client::handleBuffer(Client& client, const char buf[Server::MAX_BUF], const std::string& password, std::string& output) {
-//     if (client.state == UNAUTHENTICATED) {
-//         if (getCmd(buf) != PASS) {
-//             output = "Please enter the password\r\n";
-//             return ;
-//         }
-//         // Compare password with correct one here
-//         std::string command(buf);
-//         std::string providedPassword = command.substr(5);
-//         if (providedPassword == password) {
-//             client.state = AUTHENTICATED;
-//             output = "User authenticated\r\n";
-//         } else {
-//             output = "Incorrect password\r\n";
-//             return;
-//         }
-//     } else if (client.state == AUTHENTICATED) {
-//         // Check if user enters USER and NICK here
-//         if (getCmd(buf) == NICK) {
-//             std::string command(buf);
-//             client.nickname = command.substr(5);
-//             output = client.nickname + "\r\n";
-//         } else if (getCmd(buf) == USER) {
-//             std::string command(buf);
-//             client.username = command.substr(5);
-//             output = client.username + "\r\n";
-//         }
-//         if (!client.nickname.empty() && !client.username.empty()) {
-//             client.state = REGISTERED;
-//             output = "User registered\r\n";
-//         }
-//     } else if (client.state == REGISTERED) {
-//         // Handle normal commands here
-//         if (getCmd(buf) == PRIVMSG) {
-//             std::string command(buf);
-//             size_t pos = command.find(":");
-//             if (pos != std::string::npos) {
-//                 std::string target = command.substr(8, pos - 8 - 1);
-//                 std::string message = command.substr(pos + 1);
-//                 // TODO: Send message to target
-//                 output = "Sending message to " + target + ": " + message + "\r\n";
-//             } else {
-//                 output = "Invalid PRIVMSG format\r\n";
-//             }
-//         }
-//     }
-// }
 
 void Server::client_authen(int fd, std::string cmd)
 {
@@ -147,8 +43,9 @@ void Server::client_authen(int fd, std::string cmd)
 	else if (!cli->getRegistered())
 	{
 		const std::string& pass = cmd;
-		if (pass == _password)
+		if (pass == _password) {
 			cli->setRegistered(true);
+		}
 		else
 			_sendResponse(ERR_INCORPASS(std::string("*")), fd);
 	}
@@ -223,8 +120,10 @@ void Server::set_nickname(std::string cmd, int fd) {
 					cli->setLogedin(true);
 					_sendResponse(RPL_CONNECTED(cli->GetNickName()), fd);
 					_sendResponse(RPL_NICKCHANGE(cli->GetNickName(),cmd), fd);
-					// logMsg("set nickname function within the else conditon: {%s}", cmd.c_str());
-					// JOIN(cli->GetNickName(), fd);
+					_sendResponse(RPL_YOURHOST(cli->GetNickName()), fd);
+					_sendResponse(RPL_CREATED(cli->GetNickName()), fd);
+					_sendResponse(RPL_MYINFO(cli->GetNickName()), fd);
+					_sendResponse(RPL_ISUPPORT(cli->GetNickName()), fd);
 				}
 				else {
 					_sendResponse(RPL_NICKCHANGE(oldnick,cmd), fd);
@@ -240,8 +139,11 @@ void Server::set_nickname(std::string cmd, int fd) {
 	if(cli && cli->getRegistered() && !cli->GetUserName().empty() && !cli->GetNickName().empty() && cli->GetNickName() != "*" && !cli->GetLogedIn()) {
 		cli->setLogedin(true);
 		_sendResponse(RPL_CONNECTED(cli->GetNickName()), fd);
-		// logMsg("set nickname function: {%s}", cmd.c_str());
-		// JOIN(cli->GetNickName(), fd);
+		_sendResponse(RPL_NICKCHANGE(cli->GetNickName(),cmd), fd);
+		_sendResponse(RPL_YOURHOST(cli->GetNickName()), fd);
+		_sendResponse(RPL_CREATED(cli->GetNickName()), fd);
+		_sendResponse(RPL_MYINFO(cli->GetNickName()), fd);
+		_sendResponse(RPL_ISUPPORT(cli->GetNickName()), fd);
 	}
 }
 
@@ -251,7 +153,6 @@ void	Server::set_username(std::string& cmd, int fd)
 
 	Client *cli = GetClient(fd); 
 	if((cli && splited_cmd.size() < 5)) {
-		std::cerr << "something happened" << std::endl;
 		_sendResponse(ERR_NOTENOUGHPARAM(cli->GetNickName()), fd);
 		return;
 	}
@@ -269,7 +170,10 @@ void	Server::set_username(std::string& cmd, int fd)
 	{
 		cli->setLogedin(true);
 		_sendResponse(RPL_CONNECTED(cli->GetNickName()), fd);
-		// logMsg("setUserName fucntion: {%s} {%s}", cmd.c_str(), cli->GetNickName().c_str());
-		// JOIN(cli->GetNickName(), fd);
+		_sendResponse(RPL_NICKCHANGE(cli->GetNickName(),cmd), fd);
+		_sendResponse(RPL_YOURHOST(cli->GetNickName()), fd);
+		_sendResponse(RPL_CREATED(cli->GetNickName()), fd);
+		_sendResponse(RPL_MYINFO(cli->GetNickName()), fd);
+		_sendResponse(RPL_ISUPPORT(cli->GetNickName()), fd);
 	}
 }
